07116a2c195866b60372621402574512b9a376cf
Xenan <k.kirill@meta.studio>
2024-12-19 05:03:41 +0700

    feat: add dialog route

--- project/app/Http/Controllers/ChatController.php
+++ project/app/Http/Controllers/ChatController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\DialogDTO;
 use App\Http\Requests\CreateMessageRequest;
+use App\Http\Requests\GetDialogRequest;
+use App\Http\Resources\DialogResource;
 use App\Jobs\SendMessageByWebSocket;
+use App\Models\User;
 use App\Services\ChatService;
 use Illuminate\Http\JsonResponse;
 
@@ -16,5 +20,16 @@ class ChatController extends Controller
         $message = $this->chatService->createMessage($request->getMessageData());
         dispatch(new SendMessageByWebSocket($message));
         return response()->json(['message' => 'success'], 201);
+    }
+
+    public function getDialog(GetDialogRequest $request): DialogResource
+    {
+        $user = auth()->user();
+        $chatPartner = User::query()->find($request->input('chat_partner_id'));
+        $messages = $this->chatService->getDialogMessages($user->id, $chatPartner->id);
+
+        return new DialogResource(
+            new DialogDTO($user, $chatPartner, $messages, $chatPartner->avatar)
+        );
     }
 }

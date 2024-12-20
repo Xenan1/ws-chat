abf7efee53cef0a9a582c14dbc59f76ba3efca02
Xenan <k.kirill@meta.studio>
2024-12-20 09:53:07 +0700

    feat: add FeedResource

---
+++ project/app/Http/Resources/FeedResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+
+use App\Models\Post;
+use Illuminate\Support\Collection;
+
+class FeedResource extends BaseJsonResource
+{
+    /**
+     * @var Collection<Post>
+     */
+    protected Collection $posts;
+
+    public function __construct(Collection $posts)
+    {
+        $this->posts = $posts;
+        parent::__construct($posts);
+    }
+
+    public function toArray($request): array
+    {
+        return [
+            'posts' => $this->posts->map(function (Post $post) {
+                return [
+                    'author' => new UserResource($post->author),
+                    'text' => $post->text,
+                    'likes' => $post->getLikesCount(),
+                ];
+            })
+        ];
+    }
+}

ad11e63a945a2037ff07bf6a2e0a97ac55dc99e3
Xenan <kovalenkokd1209@gmail.com>
2024-12-27 14:48:16 +0700

    feat: optimized feed query

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Tag::factory(150)->create();
+        Post::factory(5000)->create()->each(function (Post $post) {
+            $post->tags()->attach(Tag::query()->inRandomOrder()->limit(4)->get());
+            $post->likes()->attach(User::query()->inRandomOrder()->limit(3)->get());
+        });
     }
 }
